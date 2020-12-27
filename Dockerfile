3addef974b9d70ae67449e972759d5ddbbcce94d
Milo van der Linden <milo@dogodigi.net>
2020-12-27 22:52:32 +0100

    Introducing ENV vars or .env file

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,12 @@ RUN cargo build --release
 # Copy the statically-linked binary into a apline container (cannot use scratch, we need ca-certificates)
 FROM scratch
 # RUN apk update && apk add ca-certificates && rm -rf /var/cache/apk/*
-
+ENV GIT_REPOSITORY=https://github.com/Amsterdam/fixxx-pakjekraam.git
+ENV ROCKET_SECRET_KEY=8Xui8SN4mI+7egV/9dlfYYLGQJeEx4+DwmSQLwDVXJg=
 VOLUME /tmp
 COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
 COPY --from=builder /home/rust/target/x86_64-unknown-linux-musl/release/bewerkdemarkten-api .
+
 # USER 1000
 EXPOSE 8000
 CMD ["./bewerkdemarkten-api"]
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,28 @@
 
 #[macro_use]
 extern crate rocket;
+extern crate dotenv;
 
 use git2::Repository;
-
+use dotenv::dotenv;
+use std::env;
 mod generic;
 mod market;
 
 fn main() {
-    let url = "https://github.com/Amsterdam/fixxx-pakjekraam.git";
-    match Repository::clone(url, "/tmp/fixxx-pakjekraam") {
-       Ok(_repo) => println!("Repo cloned"),
-       Err(e) => println!("Failed to clone: {}", e),
+    
+    dotenv().ok();
+
+    match env::var("GIT_REPOSITORY") {
+        Ok(val) => {
+            match Repository::clone(&val, "/tmp/fixxx-pakjekraam") {
+                Ok(_repo) => println!("{} cloned", val),
+                Err(e) => println!("Failed to clone: {}", e),
+             };
+        },
+        Err(e) => println!("couldn't interpret {}: {}", "GIT_REPOSITORY", e),
     };
+
     let mut rocket = rocket::ignite();
     rocket = generic::mount(rocket);
     rocket = market::mount(rocket);
