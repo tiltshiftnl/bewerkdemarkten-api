c357b69dcd97692bf6fb4b5cfcf20b7ced486009
milovanderlinden <milo@dogodigi.net>
2021-01-04 07:56:30 +0100

    Allow for post method

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ fn main() {
     let allowed_origins = AllowedOrigins::some_exact(&["http://localhost:3000"]);
     let cors = rocket_cors::CorsOptions {
         allowed_origins,
-        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_methods: vec![Method::Get, Method::Post].into_iter().map(From::from).collect(),
         allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
         allow_credentials: true,
         ..Default::default()
