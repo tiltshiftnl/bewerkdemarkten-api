f21ff76247ce043ee4c51ed34434fea3c31fa011
milovanderlinden <milo@dogodigi.net>
2021-01-04 08:52:35 +0100

    Allow for posting of market data

--- src/market/v1.rs
+++ src/market/v1.rs
@@ -1,7 +1,7 @@
 use bewerkdemarkten_api::models::v1::{Branche, Geography, Location, Page, Rows};
 use rocket::{self, response::Debug, Data};
 use rocket_contrib::json::Json;
-use std::{io, io::Read, fs::File};
+use std::{error::Error, fs::create_dir_all, fs::write, fs::File, io, io::Read};
 
 fn read_file(filename: String) -> String {
     match File::open(filename) {
@@ -30,6 +30,26 @@ fn get_market_day_branches(market_day: String) -> Json<Option<Vec<Branche>>> {
     })
 }
 
+#[post(
+    "/<market_day>/branches.json",
+    format = "application/json",
+    data = "<data>"
+)]
+fn post_market_day_branches(
+    market_day: String,
+    data: Json<Vec<Branche>>,
+) -> Result<Json<String>, Box<dyn Error>> {
+    let filename: String = format!(
+        "/tmp/fixxx-pakjekraam/config/markt/{}/branches.json",
+        market_day
+    );
+    let data = serde_json::to_string(&data.into_inner())?;
+    create_dir_all(format!("/tmp/fixxx-pakjekraam/config/markt/{}", market_day))?;
+    write(&filename, data)?;
+    Ok(Json("ok".to_string()))
+}
+
+
 #[get("/<market_day>/geografie.json")]
 fn get_market_day_geography(market_day: String) -> Json<Option<Geography>> {
     let market_day_geography: String = read_file(format!(
@@ -43,6 +63,25 @@ fn get_market_day_geography(market_day: String) -> Json<Option<Geography>> {
             None
         }
     })
+}
+
+#[post(
+    "/<market_day>/geografie.json",
+    format = "application/json",
+    data = "<data>"
+)]
+fn post_market_day_geography(
+    market_day: String,
+    data: Json<Option<Geography>>,
+) -> Result<Json<String>, Box<dyn Error>> {
+    let filename: String = format!(
+        "/tmp/fixxx-pakjekraam/config/markt/{}/geografie.json",
+        market_day
+    );
+    let data = serde_json::to_string(&data.into_inner())?;
+    create_dir_all(format!("/tmp/fixxx-pakjekraam/config/markt/{}", market_day))?;
+    write(&filename, data)?;
+    Ok(Json("ok".to_string()))
 }
 
 #[get("/<market_day>/locaties.json")]
@@ -60,6 +99,25 @@ fn get_market_day_locations(market_day: String) -> Json<Option<Vec<Location>>> {
     })
 }
 
+#[post(
+    "/<market_day>/locaties.json",
+    format = "application/json",
+    data = "<data>"
+)]
+fn post_market_day_locations(
+    market_day: String,
+    data: Json<Vec<Location>>,
+) -> Result<Json<String>, Box<dyn Error>> {
+    let filename: String = format!(
+        "/tmp/fixxx-pakjekraam/config/markt/{}/locaties.json",
+        market_day
+    );
+    let data = serde_json::to_string(&data.into_inner())?;
+    create_dir_all(format!("/tmp/fixxx-pakjekraam/config/markt/{}", market_day))?;
+    write(&filename, data)?;
+    Ok(Json("ok".to_string()))
+}
+
 #[get("/<market_day>/markt.json")]
 fn get_market_day_rows(market_day: String) -> Json<Option<Rows>> {
     let market_day_rows: String = read_file(format!(
@@ -73,6 +131,25 @@ fn get_market_day_rows(market_day: String) -> Json<Option<Rows>> {
             None
         }
     })
+}
+
+#[post(
+    "/<market_day>/markt.json",
+    format = "application/json",
+    data = "<data>"
+)]
+fn post_market_day_rows(
+    market_day: String,
+    data: Json<Rows>,
+) -> Result<Json<String>, Box<dyn Error>> {
+    let filename: String = format!(
+        "/tmp/fixxx-pakjekraam/config/markt/{}/markt.json",
+        market_day
+    );
+    let data = serde_json::to_string(&data.into_inner())?;
+    create_dir_all(format!("/tmp/fixxx-pakjekraam/config/markt/{}", market_day))?;
+    write(&filename, data)?;
+    Ok(Json("ok".to_string()))
 }
 
 #[get("/<market_day>/paginas.json")]
@@ -90,10 +167,27 @@ fn get_market_day_pages(market_day: String) -> Json<Option<Vec<Page>>> {
     })
 }
 
-#[get(
-    "/<market_day>/download/pdf"
+#[post(
+    "/<market_day>/paginas.json",
+    format = "application/json",
+    data = "<data>"
 )]
-fn get_market_day_pdf(market_day: String) ->  Option<File> {
+fn post_market_day_pages(
+    market_day: String,
+    data: Json<Vec<Page>>,
+) -> Result<Json<String>, Box<dyn Error>> {
+    let filename: String = format!(
+        "/tmp/fixxx-pakjekraam/config/markt/{}/paginas.json",
+        market_day
+    );
+    let data = serde_json::to_string(&data.into_inner())?;
+    create_dir_all(format!("/tmp/fixxx-pakjekraam/config/markt/{}", market_day))?;
+    write(&filename, data)?;
+    Ok(Json("ok".to_string()))
+}
+
+#[get("/<market_day>/download/pdf")]
+fn get_market_day_pdf(market_day: String) -> Option<File> {
     let filename: String = format!(
         "/tmp/fixxx-pakjekraam/dist/pdf/{}-{}.pdf",
         "kaart",
@@ -114,7 +208,6 @@ fn post_market_day_pdf(market_day: String, data: Data) -> Result<String, Debug<i
         "kaart",
         market_day.to_string()
     );
-    //data.open().take(1000000);
     data.stream_to_file(filename)
         .map(|n| n.to_string())
         .map_err(Debug)
@@ -131,6 +224,11 @@ pub fn mount(rocket: rocket::Rocket) -> rocket::Rocket {
             get_market_day_pages,
             get_market_day_pdf,
             post_market_day_pdf,
+            post_market_day_branches,
+            post_market_day_geography,
+            post_market_day_locations,
+            post_market_day_rows,
+            post_market_day_pages,
         ],
     )
 }
